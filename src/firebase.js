af82287993daa007a815d042347be8f7ca324449
Ayush <ayushkhanduri2612@gmail.com>
2023-07-25 23:24:34 +0530

    Initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnDfoXDvRpLzvBkveTyOQwFSB7dTeqSes",
-  authDomain: "chat-app-430f6.firebaseapp.com",
-  projectId: "chat-app-430f6",
-  storageBucket: "chat-app-430f6.appspot.com",
-  messagingSenderId: "136671443777",
-  appId: "1:136671443777:web:49b6fe107cc3bb67fa7fc0",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
